1455f4f08f38e1c1d649bc97a11e474d2098d27f
ahmad-majid <fa20-bscs-531@lgu.edu.pk>
2026-09-21 17:37:59 +0500

    first commit

---
+++ commands.sh
@@ -0,0 +1,25 @@
+# AWS EC2 + Linux + Nginx Practical
+
+# STEP 1: Update Ubuntu package information
+
+sudo apt update
+
+# STEP 2: Install Nginx web server
+
+sudo apt install nginx -y
+
+# STEP 6: Go to Nginx's default web directory
+
+cd /var/www/html
+
+# STEP 7: List files in the web directory
+
+ls
+
+# STEP 8: Create/edit the webpage
+
+sudo nano /var/www/html/index.html
+
+# STEP 9: Check the contents of index.html
+
+cat /var/www/html/index.html
